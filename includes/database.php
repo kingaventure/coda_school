b0166c5f4540bfcb036086dd37301675abeb163a
kingaventure <j.groult45@gmail.com>
2024-11-26 16:27:27 +0100

    p

---
+++ includes/database.php
@@ -0,0 +1,9 @@
+<?php
+ 
+ 
+ try {
+    $pdo = new PDO('mysql:host=localhost;dbname=coda_school_new','root');
+} catch (Exception $e) {
+    $errors[] = "Erreur de connexion à la bdd {$e->getMessage()}";
+}
+?>
