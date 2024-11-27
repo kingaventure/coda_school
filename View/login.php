697d49c6c8e82cc604e88b182859c832068f5ba6
kingaventure <j.groult45@gmail.com>
2024-11-27 10:40:58 +0100

    f

--- View/login.php
+++ View/login.php
@@ -1,15 +1,12 @@
-<form method="POST">
-  <div class="mb-3">
-    <label for="username" class="form-label">Username</label>
-    <input type="text" class="form-control" id="username">
-  </div>
-  <div class="mb-3">
-    <label for="pass" class="form-label">Password</label>
-    <input type="password" class="form-control" id="pass">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary" name="login_button" >Submit</button>
+<form method="post">
+    <div class="mb-3">
+        <label for="username" class="form-label">Identifiant</label>
+        <input type="text" class="form-control" name="username" id="username" required>
+    </div>
+    <div class="mb-3">
+        <label for="pass" class="form-label">Mot de passe</label>
+        <input type="password" class="form-control" name="pass" id="pass" required>
+    </div>
+
+    <button type="submit" class="btn btn-primary" name="login_button">Valider</button>
 </form>
