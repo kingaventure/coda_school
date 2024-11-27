5642f7c58642d03aafe77a4fa07f5e71264271df
kingaventure <j.groult45@gmail.com>
2024-11-27 09:28:03 +0100

    e

--- Controller/login.php
+++ Controller/login.php
@@ -1,6 +1,24 @@
 <?php   
 
     require "Model/login.php";
+
+
+    if(isset($_POST['login_button'])) {
+
+        $username = !empty($_POST['username']) ? $_POST['username'] : NULL;
+        $pass = !empty($_POST['pass']) ? $_POST['pass'] : NULL;
+
+        if(!empty($username) && !empty($pass))  {
+            
+        }
+
+    }
+
+
+
+
+
+
     require "View/login.php";
 
 ?>
